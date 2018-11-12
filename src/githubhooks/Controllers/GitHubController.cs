a00e7c146cdbee47a11bf5de8b455e9df0c588af
Aleix Lopez <aleix.lopez@carglass.es>
2018-11-12 10:56:45 +0100

    Add project files.

---
+++ src/githubhooks/Controllers/GitHubController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Security;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebHooks;
+using Newtonsoft.Json.Linq;
+using Octopus.Client;
+using Serilog;
+
+namespace githubhooks.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class GitHubController : ControllerBase
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+        private readonly OctopusServerEndpoint octopusServerEndpoint;
+        private readonly HttpClientHandler handler;
+
+        public GitHubController(IHttpClientFactory httpClientFactory, OctopusServerEndpoint octopusServerEndpoint)
+        {
+            this.httpClientFactory = httpClientFactory;
+            this.octopusServerEndpoint = octopusServerEndpoint;
+        }
+
+        [GitHubWebHook]
+        public async Task<IActionResult> GitHub(string id, JObject data)
+        {
+            
+            
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                var action = (string)data["action"];
+
+                Log.Information("GitHub Hook with {@action}", action);
+                if ((string)data["action"] == "published")
+                {
+                    var httpClient = httpClientFactory.CreateClient("GitHub");
+                    var assets = JArray.Parse(await httpClient.GetStringAsync((string)data["release"]["assets_url"]));
+                    await UploadToOctopus(httpClient, assets);
+                }
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                Log.Error(e, "Exception ocurred {e}");
+                throw e;
+            }
+
+            
+        }
+
+        private async Task UploadToOctopus(HttpClient httpClient, JArray assets)
+        {
+            Log.Information("Uploading assets to Octopus");
+            using (var octoClient = await OctopusAsyncClient.Create(octopusServerEndpoint))
+            {
+                foreach (var assetToken in assets)
+                {
+                    var assetName = (string)assetToken["name"];
+                    var assetUrl = (string)assetToken["browser_download_url"];
+                    using (var response = await httpClient.GetAsync(assetUrl))
+                    {
+                        using (var content = response.Content)
+                        {
+                            Log.Information("Uploading asset {@asset} to octopus", new { name = assetName });
+                            await octoClient.Repository.BuiltInPackageRepository.PushPackage(assetName, await content.ReadAsStreamAsync(), false);
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ src/githubhooks/Startup.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Security;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.HealthChecks;
+using Octopus.Client;
+using Polly;
+using Polly.Extensions.Http;
+
+namespace githubhooks
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .AddGitHubWebHooks();
+
+            services.AddHealthChecks(checks =>
+            {
+                checks.AddUrlCheck(Configuration["Octopus:Url"]);
+                checks.AddUrlCheck("https://www.github.com/");
+            });
+
+            services.AddScoped<OctopusServerEndpoint>(srv => new OctopusServerEndpoint(Configuration["Octopus:Url"], Configuration["Octopus:Key"]));
+
+            var policy = HttpPolicyExtensions.HandleTransientHttpError()
+                             .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+                             .WaitAndRetryAsync(5, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+
+            services.AddHttpClient("GitHub", client =>
+            {
+                client.BaseAddress = new Uri("https://api.github.com/");
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+                client.DefaultRequestHeaders.Add("User-Agent", "Carglass Hooks");
+            }).AddPolicyHandler(policy)
+              .ConfigureHttpMessageHandlerBuilder(c =>
+              {
+                  new HttpClientHandler().ServerCertificateCustomValidationCallback = (request, cert, chain, errors) =>
+                  {
+                      return errors == SslPolicyErrors.None;
+                  };
+              });
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc();
+        }
+    }
+}
